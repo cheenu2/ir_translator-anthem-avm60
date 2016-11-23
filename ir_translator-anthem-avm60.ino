e1b2fa778501c1914426f6df7b0f9170a881e1c2
Cheenu <cheenu321@gmail.com>
2016-11-23 07:20:19 -0600

    added date/time setting

--- ir_translator-anthem-avm60.ino
+++ ir_translator-anthem-avm60.ino
@@ -1,9 +1,13 @@
 #include <IRremote.h>
 #include <SoftwareSerial.h>
+#include <TimeLib.h>
 
-const int IR_RX_PIN = 12;
-const int RS232_RX_PIN = 2;
-const int RS232_TX_PIN = 3;
+#define IR_RX_PIN 12
+#define RS232_RX_PIN 2
+#define RS232_TX_PIN 3
+
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+//#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 IRrecv irrecv(IR_RX_PIN);
 SoftwareSerial anthemSerial(RS232_RX_PIN, RS232_TX_PIN); // RX, TX
@@ -21,33 +25,89 @@ void setup() {
   irrecv.enableIRIn();
   irrecv.blink13(true);
 
+  //setSyncProvider(requestTimeSync);  //set function to call when sync required
+  Serial.println("to set date/time copy/paste output of 'echo \"T$(($(date +%s)-6*60*60))\"' terminal command in serial input above");
+
   Serial.println("ready");
 
   anthemSerial.begin(115200); // open serial connection and set baud rate
 }
 
 void loop() {
+  if (Serial.available()) {
+    processTimeSyncMessage();
+  }
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
+    printDateTime(now());
+    Serial.print(" ");
+    Serial.print(F("received from remote:0x"));
+    Serial.print(results.value, HEX);
     //dump(&results);
     String anthemCommand;
     if (results.value == 0x490) {
       anthemCommand = "ZxSIM0025;";
-      Serial.println("volume up, sending " + anthemCommand);
+      Serial.println("(volume up), sending to avm60:" + anthemCommand);
       anthemSerial.println(anthemCommand);
     } else if (results.value == 0xC90) {
       anthemCommand = "ZxSIM0026;";
-      Serial.println("volume down, sending " + anthemCommand);
+      Serial.println("(volume down), sending to avm60:" + anthemCommand);
       anthemSerial.println(anthemCommand);
     } else if (results.value == 0x290) {
       anthemCommand = "ZxSIM0027;";
-      Serial.println("mute toggle, sending " + anthemCommand);
+      Serial.println("(toggle mute), sending to avm60:" + anthemCommand);
       anthemSerial.println(anthemCommand);
     } else {
-      //Serial.println(results.value, HEX);
+      //Serial.print(results.value, HEX);
+      Serial.println();
     }
     delay(175);
     irrecv.resume(); // receive the next value
   }
 }
+
+// display the given date/time
+void printDateTime(time_t t) {
+  Serial.print(padDigits(hour(t)));
+  Serial.print(":");
+  Serial.print(padDigits(minute(t)));
+  Serial.print(":");
+  Serial.print(padDigits(second(t)));
+  Serial.print("-");
+  Serial.print(month(t));
+  Serial.print("/");
+  Serial.print(day(t));
+  Serial.print("/");
+  Serial.print(year(t));
+}
+
+// utility function for digital clock display: prints preceding colon and leading 0
+String padDigits(int digits) {
+  String result;
+  if (digits < 10) {
+    result = "0" + String(digits);
+  } else {
+    result = String(digits);
+  }
+  return result;
+}
+
+void processTimeSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1451606400; // Jan 1 2016
+
+  if (Serial.find(TIME_HEADER)) {
+    pctime = Serial.parseInt();
+    if (pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+      setTime(pctime); // Sync Arduino clock to the time received on the serial port
+      Serial.print("date/time set to ");
+      printDateTime(now());
+      Serial.println();
+    }
+  }
+}
+
+//time_t requestTimeSync() {
+//  //Serial.write(TIME_REQUEST);
+//  return 0; // the time will be set later in response to serial mesg
+//}
 
